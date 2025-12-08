999f3525e8369e78f861d62ebf2fc15cf53bfcb1
K4ZED <kenzaathallah.wijaya@gmail.com>
2025-12-08 12:19:35 +0700

    update

--- src/ontology_loader.py
+++
@@ -1,9 +0,0 @@
-from rdflib import Graph
-import os
-
-def load_graph():
-    project_dir = os.path.dirname(os.path.dirname(__file__))
-    file_path = os.path.join(project_dir, "knowledge", "PotatoDisease.ttl")
-    graph = Graph()
-    graph.parse(file_path, format="turtle")
-    return graph
